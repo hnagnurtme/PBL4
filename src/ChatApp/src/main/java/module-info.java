615c4d7d690ceae4e872f2f2426b35beaf2cf93a
Trung Ánh <anhnon0106@gmail.com>
2025-09-06 00:55:41 +0700

    chore(gui-client): remove unuse file
    #18

--- src/ChatApp/src/main/java/module-info.java
+++ src/ChatApp/src/main/java/module-info.java
@@ -6,8 +6,6 @@ module com.chatapp.chatapp {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.chatapp.chatapp to javafx.fxml;
-    exports com.chatapp.chatapp;
     exports com.chatapp.chatapp.controller;
     opens com.chatapp.chatapp.controller to javafx.fxml;
 
