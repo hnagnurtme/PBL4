35e950b0d0f0fc709089c6dee84fc647a92fb521
Trung Ánh <anhnon0106@gmail.com>
2025-09-06 15:15:40 +0700

    feat(satellite-service): add view for home page
    #17

--- src/satellite-service/src/main/java/com/sagin/satellite/SatelliteApp.java
+++ src/satellite-service/src/main/java/com/sagin/satellite/SatelliteApp.java
@@ -4,12 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sagin.satellite.config.ApplicationConfiguration;
+import com.sagin.satellite.view.SatelliteWebView;
 
 public class SatelliteApp {
     private static final Logger logger = LoggerFactory.getLogger(SatelliteApp.class);
     public static void main(String[] args) {
         ApplicationConfiguration.init();
         ApplicationConfiguration.initServer(6000);
+        try {
+            SatelliteWebView.startServer(8080);
+        } catch (Exception e) {
+            logger.error("Failed to initialize Firebase: {}", e.getMessage());
+            return;
+        }
         logger.info("Satellite Service is running on port 6000");
     }
 }
